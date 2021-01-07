f7288c5cbdd168b7dff69f71efda9bbceaef2133
Prabhav Desai <desaipd1220@gmail.com>
2021-01-06 19:52:34 -0700

    Version 1.0.0

---
+++ number_plate_detection.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+####################################
+frameWidth = 640
+frameHeight = 480
+nPlateCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_russian_plate_number.xml')
+minArea = 500
+color = (255,0,255)
+####################################
+
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, 150)
+count = 0
+
+while True:
+    success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    number_plates = nPlateCascade.detectMultiScale(imgGray, 1.1, 4)
+    for(x, y, w, h) in number_plates:
+        area = h * w
+        if area > minArea:
+            cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,255), 2)
+            cv2.putText(img, "Number Plate", (x,y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, color, 2)
+            imgROI = img[y:y+h, x:x+w]
+            cv2.imshow("ROI", imgROI)
+    cv2.imshow("Result", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("venv/Scanned/NumPlate_"+str(count)+".jpg", imgROI)
+        cv2.rectangle(img, (0,200), (640,300), (0,255,0), cv2.FILLED)
+        cv2.putText(img, "Scan Saved", (150,265), cv2.FONT_HERSHEY_COMPLEX, 2, (0,0,255), 2)
+        cv2.imshow("Result", img)
+        cv2.waitKey(500)
+        count += 1
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
+
